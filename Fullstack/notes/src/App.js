31f48416e3239e6accb2db37e5d97527789a7d41
Pran-Ker <hebbarpran@gmail.com>
2022-09-22 01:51:51 +0530

    Day 10 Final

--- Fullstack/notes/src/App.js
+++ Fullstack/notes/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Main from './Main';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className='App'>
-      Notes App
+      
+      <Sidebar />
+
+      <Main />
     </div>
   );
 }
--- Fullstack/notes/src/Main.js
+++ Fullstack/notes/src/Main.js
@@ -1,5 +1,13 @@
 function Main() {
 
+    return (
+        <div className="app-main">
+            <div className="app-main-note-edit">
+                <input type="test" id="title" autoFocus />
+            </div>
+            <div className="="app-main-note-preview></div>
+        </div>
+    )
 }
 
 export default Main
