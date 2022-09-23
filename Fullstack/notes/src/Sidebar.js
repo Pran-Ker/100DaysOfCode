b61648e1a9fa7d47cac29dc8d6eecc9a073c9afd
Pran-Ker <hebbarpran@gmail.com>
2022-09-24 02:25:59 +0530

    Revert "Revert "Day 12""
    This reverts commit e13971dee1675548a3c8e4418835046484d4c31f.

--- Fullstack/notes/src/Sidebar.js
+++ Fullstack/notes/src/Sidebar.js
@@ -1,22 +1,33 @@
-function Sidebar() {
+function Sidebar({ notes, onAddNote, onDeleteNote }) {
     
     return (
         <div className="app-sidebar">
             <div className="app-sidebar-header">
-                <h1> Notes</h1>
-                <button>Add</button>
+                <h1> Notion</h1>
+                <button onClick={onAddNote}>Add</button>
             </div>
             <div className="app-sidebar-notes">
-                <div className="app-sidebar-note">
-                    <div className="sidebar-note-title">
-                        <strong>TITLE</strong>
-                        <button> Delete</button>
+
+                {notes.map((note)=> (
+
+                    <div className="app-sidebar-note">
+                        <div className="sidebar-note-title">
+                            <strong>{note.title}</strong>
+                            <button onClick={() => onDeleteNote(note.id)}>  Delete</button>
+                        </div>
+
+                        <p>{note.body && note.body.substring(0,100) + "..."}</p>
+
+                        <small className="note-meta"> Last modified {new Date(note.lastModified).toLocaleDateString("en-IN",{
+                            hour: '2-digit',
+                            minute: '2-digit',
+                        })}
+                        </small>
                     </div>
 
-                    <p> Note Preview</p>
+                ))}
 
-                    <small className="note-meta"> Last modified [date]</small>
-                </div>
+                
             </div>
         </div>
     );
