81ff929c6178f6e62f90109ad939f3a811b1206b
Pran-Ker <hebbarpran@gmail.com>
2022-10-01 01:16:44 +0530

    Blog

---
+++ 17/blog/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {useEffect, useState} from "react";
+
+import uuid from 'react-uuid';
+
+function App() {
+  
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
